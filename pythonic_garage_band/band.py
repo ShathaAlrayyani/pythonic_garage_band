ecfab316240bda612af6ecd52515aa011d3516e0
ShathaAlrayyani <shatha.alrayyani223@gmail.com>
2022-06-10 12:44:37 +0300

    building my classes

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,12 +1,44 @@
-Band,
-    Musician,
-    Guitarist,
-    Bassist,
-    Drummer,
+class Band:
+    band_list = []
+
+    def __int__(self, name, members, play_solos):
+        self.band_name = name
+        self.members = members
+        self.play_solos = play_solos
+
+    @classmethod
+    def to_list(cls):
+        pass
 
 
 class Musician:
-    def band(self):
+
+    def play_solo(self):
+        return ""
+
+    def __str__(self):
         pass
 
-    def
+    def __repr__(self):
+        pass
+
+
+class Guitarist(Musician):
+    def __str__(self):
+        print(f'My name is {Musician.name} Jett and I play guitar')
+
+
+class Bassist(Musician):
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+class Drummer(Musician):
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
